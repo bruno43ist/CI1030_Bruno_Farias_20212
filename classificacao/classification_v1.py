3ce49df943b27c88de192233343137eb644d9ded
bruno43ist <bruno43ist@yahoo.com.br>
2022-04-25 21:43:36 -0300

    ajustes

--- classificacao/classification_v1.py
+++ classificacao/classification_v1.py
@@ -14,10 +14,9 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
-from sklearn import preprocessing
+from sklearn import metrics, preprocessing, svm
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix,accuracy_score,recall_score,precision_score,make_scorer
+from sklearn.metrics import confusion_matrix,accuracy_score,recall_score,precision_score,make_scorer, classification_report, precision_recall_curve
 from sklearn.utils import resample, shuffle
 
 def load_data(nome_arquivo, drop_label):
@@ -25,7 +24,7 @@ def load_data(nome_arquivo, drop_label):
 
   row, col = input_data.shape
   logging.info(f'Linhas: {row} Colunas: {col}') 
-  logging.info(input_data.head(10))
+  #logging.info(input_data.head(10))
 
   #salva a classificação
   y = input_data['Class'].tolist()
@@ -47,10 +46,6 @@ def load_data(nome_arquivo, drop_label):
   #logging.info(input_data_scaled.describe())
 
   return input_data_scaled, y
-
-
-
-
 
 def plot_matriz_conf(cm, alg):
   ax = sns.heatmap(cm, annot=True, cmap='Blues')
@@ -96,14 +91,14 @@ def resample_f(data):
   #separa a classe de minoria (fraude) em um dataframe diferente
   df_1 = data[data['Class'] == 1]
 
-  logging.info('df_1>')
-  logging.info(df_1.head(10))
+  #logging.info('df_1>')
+  #print(df_1.head(10))
 
   #separa a classe de maioria (não fraude) em um dataframe diferente
   other_df = data[data['Class'] != 1]
 
-  logging.info('other_df>')
-  logging.info(other_df.head(10))
+  #logging.info('other_df>')
+  #print(other_df.head(10))
 
   #upsample a classe de minoria -> 1 amostra fraude para cada 5 amostras não fraude
   #df_1_upsampled = resample(df_1, n_samples=5*len(other_df), replace=True)
@@ -118,11 +113,11 @@ def resample_f(data):
   #tira o index
   df_1_upsampled.drop(['index'], axis=1, inplace=True)
   
-  logging.info('df_1_upsampled>')
-  logging.info(df_1_upsampled.head(10))
+  #logging.info('df_1_upsampled>')
+  #print(df_1_upsampled.head(10))
 
-  logging.info('df_1_upsampled>')
-  logging.info(df_1_upsampled)
+  #logging.info('df_1_upsampled>')
+  #print(df_1_upsampled)
 
   d1_fraude_end = df_1_upsampled[df_1_upsampled['Class'] == 1]
   d1_nao_fraude_end = df_1_upsampled[df_1_upsampled['Class'] != 1]
@@ -136,8 +131,8 @@ def resample_f(data):
   #tira as classificacoes
   df_1_upsampled.drop(['Class'], axis=1, inplace=True)
 
-  logging.info('Ao final de resample: df_1_upsampled>')
-  logging.info(df_1_upsampled.head(10))
+  #logging.info('Ao final de resample: df_1_upsampled>')
+  #print(df_1_upsampled.head(10))
 
   logging.info('Final do resample!')
 
@@ -168,7 +163,53 @@ def grid_search_wrapper(clf, param_grid, scorers, X_train, X_test, y_train, y_te
 
   return grid_search
 
-def randomForest2(data, targets, ajusteLimiar=True):
+#funções auxiliares para geração de curvas ROC e P/R
+
+#ajusta as predições das classes baseado no limite de previsão
+def adjusted_classes(y_scores, t):
+  return [1 if y >= t else 0 for y in y_scores]
+
+#plota a curva de predição recall e mostra o valor atual para cada
+#ao identificar o limite do classificador
+def precision_recall_threshold(p, r, thresholds, y_scores, y_test, t=0.5):
+  y_pred_adj = adjusted_classes(y_scores, t)
+  print(pd.DataFrame(confusion_matrix(y_test, y_pred_adj), 
+                     columns=['pred_neg', 'pred_pos'],
+                     index=['neg', 'pos']))
+
+  #plota a curva
+  plt.figure(figsize=(8,8))
+  plt.title('Curva de precisão e recall')
+  plt.step(r, p, step='post', alpha=0.2, color='b')
+  plt.xlim([0.5, 1.01])
+  plt.ylim([0.5, 1.01])
+  plt.xlabel('Recall')
+  plt.ylabel('Precisão')
+
+  #plota o limite atual na linha
+  close_default_clf = np.argmin(np.abs(thresholds - t))
+  plt.plot(r[close_default_clf], p[close_default_clf], '^', c='k', markersize=15)
+
+  nomeFig = 'curvas_'+alg+'.png'
+  plt.savefig(nomeFig)
+  logging.info('Salvo em ' + nomeFig2)
+
+  #limpa a figura para não plotar uma em cima da outra
+  plt.clf()
+
+
+#gera as curvas ROC e P/R
+def curvas(grid_search_clf, X_test, y_test, tipoAlg):
+  logging.info('Gerando curvar para o algorimo ' + tipoAlg)
+  y_scores = grid_search_clf.predict_proba(X_test)[:, 1]
+
+  p, r, thresholds = precision_recall_curve(y_test, y_scores)
+
+  precision_recall_threshold(p, r, thresholds, y_scores, y_test, 0.9)
+
+
+
+def randomForest2(data, targets, X_train, X_test, y_train, y_test, ajusteLimiar=True,):
   logging.info('')
   logging.info('')
   logging.info('')
@@ -178,25 +219,7 @@ def randomForest2(data, targets, ajusteLimiar=True):
 
   inicio = datetime.datetime.now();
 
-  #dividir o dataset entre treinamento e validação
-  logging.info('Separando dataset em teste e treinamento...')
-  X_train, X_test, y_train, y_test = train_test_split(data, targets, stratify=targets)
-
-  logging.info('======= Distribuiçao de classes (y_train) ===========')
-  logging.info(y_train.value_counts(normalize=True))
-  logging.info('')
-  logging.info(y_train.value_counts())
-  logging.info('======= Distribuiçao de classes (y_test) ===========')
-  logging.info(y_test.value_counts(normalize=True))
-  logging.info('')
-  logging.info(y_test.value_counts())
-
-  #normalizando as amostras
-  logging.info('Normalizando as amostras...')
-  sc = StandardScaler()
-  X_train = sc.fit_transform(X_train)
-  X_test = sc.transform(X_test)
-
+  #melhores parâmetros gerados em execuções anteriores do ajuste de limiar
   best_n_estimators = 10
   best_min_samples_split = 5
   best_max_depth = 15
@@ -205,6 +228,25 @@ def randomForest2(data, targets, ajusteLimiar=True):
   if ajusteLimiar:
     #fazendo ajuste de limiar de decisão
     logging.info('Rodando ajuste de limiar...')
+
+    #dividir o dataset entre treinamento e validação
+    logging.info('Separando dataset em teste e treinamento...')
+    X_train, X_test, y_train, y_test = train_test_split(data, targets, stratify=targets)
+
+    logging.info('======= Distribuiçao de classes (y_train) ===========')
+    print(y_train.value_counts(normalize=True))
+    logging.info('')
+    print(y_train.value_counts())
+    logging.info('======= Distribuiçao de classes (y_test) ===========')
+    print(y_test.value_counts(normalize=True))
+    logging.info('')
+    print(y_test.value_counts())
+
+    #normalizando as amostras
+    logging.info('Normalizando as amostras...')
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
     #(100 árvores e todos os processadores/núcleos)
     clf=RandomForestClassifier(n_jobs=-1)
@@ -230,6 +272,7 @@ def randomForest2(data, targets, ajusteLimiar=True):
 
     inicioGridSearch = datetime.datetime.now();
 
+    logging.info('Rodando GridSearchCV...')
     grid_search_clf = grid_search_wrapper(clf, param_grid, scorers, X_train, X_test, y_train, y_test, refit_score='precision_score')
 
     best_n_estimators = grid_search_clf.best_params_['n_estimators']
@@ -247,12 +290,22 @@ def randomForest2(data, targets, ajusteLimiar=True):
     finalGridSearch = datetime.datetime.now();
     tempoPassadoGridSearch = finalGridSearch - inicioGridSearch
     logging.info('Tempo passado: %d microsegundos | %d segundos' % (tempoPassadoGridSearch.microseconds, tempoPassadoGridSearch.seconds))
+    curvas(grid_search_clf, X_test, y_test, 'RF')
+
+
+  #normalizando as amostras
+  # logging.info('Normalizando as amostras...')
+  # sc = StandardScaler()
+  # X_train = sc.fit_transform(X_train)
+  # X_test = sc.transform(X_test)
 
 
   logging.info('Rodando randomForest para as configuracoes escolhidas...')
-  
-  clf=RandomForestClassifier(n_estimators=best_n_estimators, min_samples_split=best_min_samples_split, max_depth=best_max_depth, max_features=best_max_features, n_jobs=-1)
-  #clf=RandomForestClassifier(n_estimators=10, min_samples_split=3, n_jobs=-1)
+  clf=RandomForestClassifier(n_estimators=best_n_estimators, 
+                             min_samples_split=best_min_samples_split, 
+                             max_depth=best_max_depth, 
+                             max_features=best_max_features, 
+                             n_jobs=-1)
 
   #treinar o modelo usando os sets y_pred=clf.predict(X_test)
   clf.fit(X_train,y_train)
@@ -269,23 +322,25 @@ def randomForest2(data, targets, ajusteLimiar=True):
   cm = confusion_matrix(y_test, y_pred)
 
   logging.info("Confusion Matrix:")
-  logging.info(cm)
+  print(cm)
 
   #métrica de acurácia
   ac = accuracy_score(y_test,y_pred)
 
   logging.info("Accuracy score:")
-  print (ac)
+  print(ac)
 
   #recall
   rc = recall_score(y_test, y_pred)
 
   logging.info("Recall:")
-  logging.info(rc)
+  print(rc)
 
   plot_matriz_conf(cm, 'RF')
 
-def knn(data, targets, ajusteLimiar):
+  print(classification_report(y_test, y_pred))
+
+def knn(data, targets, X_train, X_test, y_train, y_test, ajusteLimiar):
 
   logging.info('')
   logging.info('')
@@ -296,30 +351,30 @@ def knn(data, targets, ajusteLimiar):
 
   inicio = datetime.datetime.now();
 
-  #dividir o dataset entre treinamento e validação
-  logging.info('Separando dataset em teste e treinamento...')
-  X_train, X_test, y_train, y_test = train_test_split(data, targets, stratify=targets)
-
-  logging.info('======= Distribuiçao de classes (y_train) ===========')
-  logging.info(y_train.value_counts(normalize=True))
-  logging.info('')
-  logging.info(y_train.value_counts())
-  logging.info('======= Distribuiçao de classes (y_test) ===========')
-  logging.info(y_test.value_counts(normalize=True))
-  logging.info('')
-  logging.info(y_test.value_counts())
-
-  #normalizando as amostras
-  logging.info('Normalizando as amostras...')
-  sc = StandardScaler()
-  X_train = sc.fit_transform(X_train)
-  X_test = sc.transform(X_test)
-
-  best_n_neighbors = 5;
+  best_n_neighbors = 1;
 
   if ajusteLimiar:
     #fazendo ajuste de limiar de decisão
     logging.info('Rodando ajuste de limiar...')
+
+    #dividir o dataset entre treinamento e validação
+    logging.info('Separando dataset em teste e treinamento...')
+    X_train, X_test, y_train, y_test = train_test_split(data, targets, stratify=targets)
+
+    logging.info('======= Distribuiçao de classes (y_train) ===========')
+    logging.info(y_train.value_counts(normalize=True))
+    logging.info('')
+    logging.info(y_train.value_counts())
+    logging.info('======= Distribuiçao de classes (y_test) ===========')
+    logging.info(y_test.value_counts(normalize=True))
+    logging.info('')
+    logging.info(y_test.value_counts())
+
+    #normalizando as amostras
+    logging.info('Normalizando as amostras...')
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
 
     #(100 árvores e todos os processadores/núcleos)
     clf=KNeighborsClassifier(n_jobs=-1)
@@ -349,14 +404,14 @@ def knn(data, targets, ajusteLimiar):
     finalGridSearch = datetime.datetime.now();
     tempoPassadoGridSearch = finalGridSearch - inicioGridSearch
     logging.info('Tempo passado: %d microsegundos | %d segundos' % (tempoPassadoGridSearch.microseconds, tempoPassadoGridSearch.seconds))
-
+    curvas(grid_search_clf, X_test, y_test, 'KNN')
 
   #aplica o KNN com a melhor quantidade de vizinhos q o ajuste definiu
+  logging.info('Rodando KNN para as configuracoes escolhidas...')
   #classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
   classifier = KNeighborsClassifier(n_neighbors = best_n_neighbors)
   classifier.fit(X_train, y_train)
 
-  #testar com os 30% separados para teste
   y_pred = classifier.predict(X_test)
 
   final = datetime.datetime.now();
@@ -384,6 +439,106 @@ def knn(data, targets, ajusteLimiar):
   logging.info(rc)
 
   plot_matriz_conf(cm, 'KNN')
+
+  print(classification_report(y_test, y_pred))
+  
+
+def svm_f(data, targets, ajusteLimiar):
+  logging.info('')
+  logging.info('')
+  logging.info('')
+  logging.info('')
+  logging.info('')
+  logging.info('Fazendo SVM...')
+
+  inicio = datetime.datetime.now();
+
+  if ajusteLimiar:
+    #fazendo ajuste de limiar de decisão
+    logging.info('Rodando ajuste de limiar...')
+
+    #dividir o dataset entre treinamento e validação
+    logging.info('Separando dataset em teste e treinamento...')
+    X_train, X_test, y_train, y_test = train_test_split(data, targets, stratify=targets)
+
+    logging.info('======= Distribuiçao de classes (y_train) ===========')
+    logging.info(y_train.value_counts(normalize=True))
+    logging.info('')
+    logging.info(y_train.value_counts())
+    logging.info('======= Distribuiçao de classes (y_test) ===========')
+    logging.info(y_test.value_counts(normalize=True))
+    logging.info('')
+    logging.info(y_test.value_counts())
+
+    #normalizando as amostras
+    logging.info('Normalizando as amostras...')
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+
+    #(100 árvores e todos os processadores/núcleos)
+    clf=KNeighborsClassifier(n_jobs=-1)
+
+    param_grid = {
+      'n_neighbors': [1, 3, 5], #número de vizinhos
+    }
+
+    #apenas para acelerar os testes
+    # param_grid = {
+    #   'n_neighbors': [1], #número de vizinhos
+    # }
+
+    scorers = {
+      'precision_score': make_scorer(precision_score), #habilidade de não classificar como positivo uma amostra que é negativa
+      'recall_score': make_scorer(recall_score), #habilidade de encontrar todas as amostras positivas
+      'accuracy_score': make_scorer(accuracy_score) # fração das previsões que o modelo acertou
+    }
+
+    inicioGridSearch = datetime.datetime.now();
+
+    grid_search_clf = grid_search_wrapper(clf, param_grid, scorers, X_train, X_test, y_train, y_test, refit_score='precision_score')
+
+    best_n_neighbors = grid_search_clf.best_params_['n_neighbors']
+    logging.info('Melhor n_neighbors: {}'.format(best_n_neighbors))
+
+    finalGridSearch = datetime.datetime.now();
+    tempoPassadoGridSearch = finalGridSearch - inicioGridSearch
+    logging.info('Tempo passado: %d microsegundos | %d segundos' % (tempoPassadoGridSearch.microseconds, tempoPassadoGridSearch.seconds))
+    curvas(grid_search_clf, X_test, y_test, 'SVM')
+
+  logging.info('Fazendo SVM...')
+  classifier = svm.SVC(kernel='linear')
+  classifier.fit(X_train, y_train)
+
+  y_pred = classifier.predict(X_test)
+
+  final = datetime.datetime.now();
+
+  tempoPassado = final - inicio
+
+  logging.info('Tempo passado: %d microsegundos | %d segundos' % (tempoPassado.microseconds, tempoPassado.seconds))
+
+  #matriz de confusão
+  cm = confusion_matrix(y_test, y_pred)
+
+  logging.info("Confusion Matrix:")
+  logging.info(cm)
+
+  #métrica de acurácia
+  ac = accuracy_score(y_test,y_pred)
+
+  logging.info("Accuracy score:")
+  print (ac)
+
+  #recall
+  rc = recall_score(y_test, y_pred)
+
+  logging.info("Recall:")
+  logging.info(rc)
+
+  plot_matriz_conf(cm, 'svm')
+
+  print(classification_report(y_test, y_pred))
   
 
 
@@ -408,11 +563,41 @@ def exporacao():
   logging.info('2. Fazendo resample...')
   data_scaled, targets = resample_f(data_scaled)
 
-  #logging.info('3. Fazendo RandomForest...')
-  randomForest2(data_scaled, targets, True)
+  #Com base nos melhores parâmetros gerados para cada algoritmo:
 
-  logging.info('3. Fazendo KNN...')
-  knn(data_scaled, targets, True)
+  #Treine modelos usando separação do DATASET em 50/50 e 80/20;
+  X_train1, X_test1, y_train1, y_test1 = train_test_split(data_scaled, targets, test_size = 0.5) #50/50
+  X_train2, X_test2, y_train2, y_test2 = train_test_split(data_scaled, targets, test_size = 0.2) #80/20
+
+  #normalizando as amostras
+  logging.info('Normalizando as amostras...')
+  sc = StandardScaler()
+
+  #50/50
+  X_train1 = sc.fit_transform(X_train1)
+  X_test1 = sc.transform(X_test1)
+
+  #80/20
+  X_train2 = sc.fit_transform(X_train2)
+  X_test2 = sc.transform(X_test2)
+
+  logging.info('3.  Fazendo RandomForest:')
+  logging.info('3.1 Fazendo RandomForest com 50/50: ')
+  randomForest2(data_scaled, targets, X_train1, X_test1, y_train1, y_test1, True)
+  logging.info('3.2 Fazendo RandomForest com 80/20: ')
+  randomForest2(data_scaled, targets, X_train2, X_test2, y_train2, y_test2, True)
+
+  logging.info('4.  Fazendo KNN...')
+  logging.info('4.1 Fazendo KNN com 50/50: ')
+  knn(data_scaled, targets, X_train1, X_test1, y_train1, y_test1, True)
+  logging.info('4.2 Fazendo KNN com 80/20: ')
+  knn(data_scaled, targets, X_train2, X_test2, y_train2, y_test2, True)
+
+  logging.info('5.  Fazendo SVM...')
+  logging.info('5.1 Fazendo SVM com 50/50: ')
+  svm_f(data_scaled, targets, X_train1, X_test1, y_train1, y_test1, False)
+  logging.info('5.2 Fazendo SVM com 80/20: ')
+  svm_f(data_scaled, targets, X_train2, X_test2, y_train2, y_test2, False)
 
 if __name__ == "__main__":
   exporacao()
